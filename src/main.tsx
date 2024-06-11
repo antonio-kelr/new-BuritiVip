ed1c262c94ffd02042a9c629a46bb0a50972c8d6
Clenilson Lopes <lorimweb@gmail.com>
2024-06-11 20:41:27 -0300

    mudanças na estrutura de pastas

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/saga-blue/theme.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Admin from "./components/adminn/admin.tsx";
 import App from "./App.tsx";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="/admin" element={<Admin/>} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
